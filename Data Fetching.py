27b8779aaa59975f244192a63af3e908857f1334
Toma Nadu <toma.nadu@protonmail.com>
2022-05-11 20:03:57 +0300

    Project completed

--- Data Fetching.py
+++ Data Fetching.py
@@ -5,13 +5,14 @@ import re
 import time
 
 # Hard-coded list of OLX URLs
-pages = [f'https://www.olx.ro/imobiliare/?page={i}' for i in range(1, 26)]
+pages = [f'https://www.olx.ro/imobiliare/?page={i}' for i in range(1, 6)]
 
 # Lists which will store ads corresponding to each category
-apartments=[]
-singleRooms=[]
-houses=[]
-land=[]
+apartments = []
+singleRooms = []
+houses = []
+land = []
+
 
 # Modular functions for each and every regex search performed
 # Category REGEX
@@ -23,20 +24,21 @@ def isHouse(title, description):
 
 
 def isApartment(title, description):
-    regex = re.compile(r'apart[ae]\*ment|ap\s+|apart\s+|studio', re.IGNORECASE)
+    regex = re.compile(r'apart[ae]*ment|\bap(art)?\b', re.IGNORECASE)
     if re.search(regex, title) or re.search(regex, description):
         return True
     return False
 
 
 def isSingleRoom(title, description):
-    regex=re.compile(r'(garsonier[aăe]|studio)', re.IGNORECASE)
+    regex = re.compile(r'garsonier[aăe]|studio', re.IGNORECASE)
     if regex.search(title) or regex.search(description):
         return True
     return False
 
+
 def isLand(title, description):
-    regex=re.compile('teren', re.IGNORECASE)
+    regex = re.compile('teren', re.IGNORECASE)
     if re.search(regex, title) or re.search(regex, description):
         return True
     return False
@@ -44,14 +46,14 @@ def isLand(title, description):
 
 # Transaction type REGEX
 def isForSale(title, description):
-    regex = re.compile(r'v[aîăâ]nzare|v[aîăâ]nd|sale', re.IGNORECASE)
+    regex = re.compile(r'v[aîăâ]nzare|v[aîăâ]nd|sale|achizi[tț]i(e|on[ăa]r[ei])', re.IGNORECASE)
     if re.search(regex, title) or re.search(regex, description):
         return True
     return False
 
 
 def isForRental(title, description):
-    regex=re.compile(r'([iî]n)?\s*chiri(e|ez|at|[aă]m|ere)', re.IGNORECASE)
+    regex = re.compile(r'([iîâ]n)?\s*chiri(e|ez|at|[aă]m|ere)', re.IGNORECASE)
     if regex.search(title) or regex.search(description):
         return True
     return False
@@ -66,7 +68,7 @@ def isForExchange(title, description):
 
 # Sub-filtering REGEX
 def getRooms(title, description):
-    regex = re.compile(r'[\do]+\s\*camer[eăa]|[\do]+\s\*cam|[\do]+\s\*[iâî]nc[aă]per[ei]', re.IGNORECASE)
+    regex = re.compile(r'[\do]+\s*camer[eăa]|[\do]+\s*cam|[\do]+\s*[iâî]nc[aă]per[ei]', re.IGNORECASE)
 
     if re.search(regex, title):
         return int("".join([x for x in regex.findall(title)[0] if x.isdigit()]))
@@ -77,7 +79,7 @@ def getRooms(title, description):
 
 
 def getYearOfConstruction(title, description):
-    regex=re.compile(r'an(ul)?\s*(20[12]\d|19\d{2})', re.IGNORECASE)
+    regex = re.compile(r'an(ul)?\s*(20[12]\d|19\d{2})', re.IGNORECASE)
     number = re.compile('\d+')
 
     if regex.search(title):
@@ -89,13 +91,14 @@ def getYearOfConstruction(title, description):
 
     return False
 
+
 def getSurface(title, description):
-    regexMp=re.compile(r'\d+\s*m(p|\^?2|²|p[aă]tra[tţ]i*|etr[iu]+\s*p|etr[iu]+\s*p[aă]tra[tţ]i*)', re.IGNORECASE)
-    regexHa=re.compile(r'\d+\s*h(a|ectar[ie]*)', re.IGNORECASE)
+    regexMp = re.compile(r'\d+\s*m(p|\^?2|²|p[aă]tra[tţ]i*|etr[iu]+\s*p|etr[iu]+\s*p[aă]tra[tţ]i*)', re.IGNORECASE)
+    regexHa = re.compile(r'\d+\s*h(a|ectar[ie]*)', re.IGNORECASE)
     number = re.compile('\d+')
 
     if regexMp.search(title):
-        value=int(number.search(regexMp.search(title).group()).group())
+        value = int(number.search(regexMp.search(title).group()).group())
         return f'{value} m²'
     elif regexMp.search(description):
         value = int(number.search(regexMp.search(description).group()).group())
@@ -111,11 +114,10 @@ def getSurface(title, description):
 
 
 # returns a properly formatted string, using all transaction type regex functions
-def getTransactionType(title,description):
+def getTransactionType(title, description):
     rental = isForRental(title, description)
     sale = isForSale(title, description)
     exchange = isForExchange(title, description)
-
 
     if (rental and sale) or (rental and exchange) or (sale and exchange):
         # Multiple transaction types found (conflict), so transaction type is considered undecidable
@@ -127,6 +129,7 @@ def getTransactionType(title,description):
     if exchange:
         return 'For exchange'
 
+
 # Main filtering function
 # - receives source ad URL, extracted title and description
 # - appends formatted result, based on source ad URL and regex filters, to corresponding category list
@@ -137,31 +140,33 @@ def filters(ad, title, description):
     global land
 
     if isApartment(title, description):
-        result=getTransactionType(title, description)
+        result = getTransactionType(title, description)
 
-        surface=getSurface(title, description)
+        surface = getSurface(title, description)
         if surface and result:
-            result+=f' - surface: {surface}'
+            result += f' - surface: {surface}'
         elif surface:
-            result+=f'Surface: {surface}'
+            result += f'Surface: {surface}'
 
-        year=getYearOfConstruction(title, description)
+        year = getYearOfConstruction(title, description)
         if year and result:
-            result+=f' - year of construction: {year}'
+            result += f' - year of construction: {year}'
         elif year:
-            result+=f'Year of construction: {year}'
+            result += f'Year of construction: {year}'
 
-        rooms=getRooms(title, description)
+        rooms = getRooms(title, description)
         # if apartment has only one room, it is considered a single room
-        if rooms==1 and result:
+        if rooms == 1 and result:
             singleRooms.append(f'{ad}\n\t{result}')
-        elif rooms==1:
+        elif rooms == 1:
             singleRooms.append(f'{ad}\n\tNo further information could be extracted!')
         # else it is considered a regular apartment
-        elif rooms>1 and result:
+        elif rooms > 1 and result:
             apartments.append(f'{ad}\n\t{result} - {rooms} rooms')
-        elif rooms>1:
+        elif rooms > 1:
             apartments.append(f'{ad}\n\t{rooms} rooms')
+        elif result:
+            apartments.append(f'{ad}\n\t{result}')
         else:
             apartments.append(f'{ad}\n\tNo further information could be extracted!')
 
@@ -202,17 +207,11 @@ def filters(ad, title, description):
         elif year:
             result += f'Year of construction: {year}'
 
-        rooms=getRooms(title, description)
+        rooms = getRooms(title, description)
         if rooms and result:
-            result+=f' - {rooms} rooms'
+            result += f' - {rooms} rooms'
         elif rooms:
-            result+=f'{rooms} rooms'
-
-        floors=getFloors(title, description)
-        if floors and result:
-            result+=f' - {floors} floors'
-        elif floors:
-            result+=f'{floors} floors'
+            result += f'{rooms} rooms'
 
         if result:
             houses.append(f'{ad}\n\t{result}')
@@ -238,34 +237,27 @@ def filters(ad, title, description):
 # Function used for extracting individual ad pages from parent ad-listing pages
 # - is separate for multi-threading optimization
 def adScraper(ad):
-    # global counter
-
     # request.get(URL).content returns the html file of the URL link, which is then transformed
     # into a BeautifulSoup object, using the LXML processor (for better performance)
     adPage = BeautifulSoup(requests.get(ad).content, 'lxml')
-    try:    # OLX page structure
+    try:  # OLX page structure
         title = adPage.select('h1.css-r9zjja-Text.eu5v0x0')[0].text
         description = adPage.select('div.css-g5mtbi-Text')[0].text
         filters(ad, title, description)
-        # counter += 1
-    except:     # 'Storia' page structure
-        title = adPage.select('h1.css-11kn46p.eu6swcv17')[0].text
+    except:  # 'Storia' page structure
+        title = adPage.select('h1.css-11kn46p.eu6swcv19')[0].text
         description = adPage.select('section.css-1vfwbw8.e1r1048u3')[0]
         description = description.find(class_='e1r1048u1').text
         filters(ad, title, description)
-        # print(description)
-        # counter += 1
+
 
 print('Crawling and filtering initiated.')
-time.sleep(1.5)
-print('\nThe process is expected to take about a minute.')
-time.sleep(1.5)
-print('\nPlease wait...')
+time.sleep(1)
+print('The process is expected to take a few minutes.')
+time.sleep(1)
+print('Please wait...')
 
 for page in pages:
-    # global counter
-    # print(page)
-
     # request.get(URL).content returns the html file of the URL link, which is then transformed
     # into a BeautifulSoup object, using the LXML processor (for better performance)
     links = BeautifulSoup(requests.get(page).content, 'lxml')
@@ -279,31 +271,29 @@ for page in pages:
     # [5:] because the first 5 ads are always promoted (and can show up on other ad-listing pages)
     ads = list(set(ads))[5:]
 
-    # counter = 0
-
     # spawning threads for each ad URL found on current ad-listing page (improves performance by 12 times)
     with concurrent.futures.ThreadPoolExecutor(max_workers=len(ads)) as executor:
         executor.map(adScraper, ads)
-    # print(counter)
 
 print('Done!')
-time.sleep(2)
+time.sleep(1)
 
-print('\nApartments:\n')
+print(f'\nApartments - {len(apartments)} result(s):\n')
+time.sleep(2)
 for apartment in apartments:
     print(f'\t{apartment}')
-time.sleep(2)
 
-print('\nSingle Rooms:\n')
+print(f'\nSingle Rooms - {len(singleRooms)} result(s):\n')
+time.sleep(2)
 for singleRoom in singleRooms:
     print(f'\t{singleRoom}')
-time.sleep(2)
 
-print('\nHouses:\n')
+print(f'\nHouses - {len(houses)} result(s):\n')
+time.sleep(2)
 for house in houses:
     print(f'\t{house}')
-time.sleep(2)
 
-print('\nLand:\n')
+print(f'\nLand - {len(land)} result(s):\n')
+time.sleep(2)
 for pieceOfLand in land:
     print(f'\t{pieceOfLand}')
