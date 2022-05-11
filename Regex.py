a46b192d75e1695d1a0de0400a1c9fe5352be688
Toma Nadu <toma.nadu@protonmail.com>
2022-05-11 19:14:52 +0300

    Completed

--- Regex.py
+++ Regex.py
@@ -4,3 +4,15 @@ title = ''
 description = ''
 
 
+def isHouse(title, description):
+    regex = re.compile('vil[aă]|cas[aă]|duplex|house', re.IGNORECASE)
+    if regex.search(title) or regex.search(description):
+        return True
+    return False
+
+
+def isForExchange(title, description):
+    regex = re.compile('schimb|troc', re.IGNORECASE)
+    if regex.search(title) or regex.search(description):
+        return True
+    return False
